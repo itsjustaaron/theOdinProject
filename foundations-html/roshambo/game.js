dc2a4bd692ff7d030a036426a856e82688715a2c
Aaron Childers <aaron.lee.childers@gmail.com>
2021-09-14 11:19:03 -0400

    set up directory for rock paper scissors

---
+++ foundations-html/roshambo/game.js
@@ -0,0 +1,7 @@
+// rock paper scissors
+
+// need function to emualate computer player's action
+
+// need function to compare user's action to computer's action
+// function should accept case-insensitive arguments
+// return a string that declares the winner: "You win! Rock beats scissors!"
