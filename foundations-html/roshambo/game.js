0754a46f2215b79a962c1a401a285df3cf5a788d
Aaron Childers <aaron.lee.childers@gmail.com>
2021-09-14 18:06:30 -0400

    refactor game to include user interface

--- foundations-html/roshambo/game.js
+++ foundations-html/roshambo/game.js
@@ -1,5 +1,10 @@
 // rock paper scissors
 const options = ['rock', 'paper', 'scissors'];
+const resultsContainer = document.querySelector('.results');
+const buttons = document.querySelectorAll('.game-buttons button');
+const resetButton = document.querySelector('.reset');
+let playerScore = 0;
+let computerScore = 0;
 
 // need function to emualate computer player's action
 function computerPlay() {
@@ -7,73 +12,77 @@ function computerPlay() {
   return options[randomNumber];
 }
 
-function cleanInput(value) {
-  return typeof value === 'string' ? value.toLowerCase() : null;
-}
-
 // need function to compare user's action to computer's action
 // function should accept case-insensitive arguments
 // return a string that declares the winner: "You win! Rock beats scissors!"
 function playRound(playerAction, computerAction) {
-  // compare user inputs
-  if (playerAction === computerAction) {
-    // unclear on draws - currently gives point to computer
-    // should they instead not count towards the total?
-    return 0;
-  }
-  // rock beats scissors, paper beats rock, scissors beats paper
-  // commented out to rework output
-  // let outcome = 'lose';
-  // if (
-  //   (playerAction === 'rock' && computerAction === 'scissors') ||
-  //   (playerAction === 'scissors' && computerAction === 'paper') ||
-  //   (playerAction === 'paper' && computerAction === 'rock')
-  // ) {
-  //   outcome = 'win';
-  // }
-
-  // return `You ${outcome}! ${
-  //   outcome === 'win' ? playerAction : computerAction
-  // } beats ${outcome === 'win' ? computerAction : playerAction}`;
-
-  // version two
-  let outcome = false;
-  if (
-    (playerAction === 'rock' && computerAction === 'scissors') ||
-    (playerAction === 'scissors' && computerAction === 'paper') ||
-    (playerAction === 'paper' && computerAction === 'rock')
-  ) {
-    outcome = true;
-  }
-
-  return outcome;
-}
-
-// need function to play 5 rounds and determine winner at the end
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let drawCount = 0;
-
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < 5; i++) {
-    const player = cleanInput(prompt('Rock? Paper? Scissors?'));
-    const playerWins = playRound(player, computerPlay());
-    if (playerWins) {
+  if (playerScore < 5 && computerScore < 5) {
+    const roundResults = document.createElement('p');
+    // compare user inputs
+    if (playerAction === computerAction) {
+      // unclear on draws - currently gives no points
+      roundResults.textContent = "It's a draw!";
+    } else if (
+      (playerAction === 'rock' && computerAction === 'scissors') ||
+      (playerAction === 'scissors' && computerAction === 'paper') ||
+      (playerAction === 'paper' && computerAction === 'rock')
+    ) {
+      roundResults.textContent = 'Player wins!';
       playerScore += 1;
-      console.log(`Player wins round ${i + 1}`);
-    } else if (!playerWins && playerWins !== 0) {
-      computerScore += 1;
-      console.log(`Computer wins round ${i + 1}`);
     } else {
-      drawCount += 1;
-      console.log(`Draw! Player and computer both played ${player}.`);
+      roundResults.textContent = 'Computer wins!';
+      computerScore += 1;
+    }
+
+    resultsContainer.appendChild(roundResults);
+
+    // end game once someone reaches 5 points
+    if (playerScore === 5 || computerScore === 5) {
+      // prevent further input
+      buttons.forEach(button => {
+        button.setAttribute('disabled', true);
+      });
+      // then alert the winner
+      alert(
+        `${playerScore > computerScore ? 'Player' : 'Computer'} wins the game!`
+      );
     }
   }
-
-  return `${
-    playerScore > computerScore ? 'Player' : 'Computer'
-  } wins! The final score is: Player - ${playerScore} | Computer - ${computerScore} | Draws - ${drawCount}`;
 }
 
-game();
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+
+  buttons.forEach(button => {
+    button.removeAttribute('disabled');
+  });
+
+  resultsContainer.innerHTML = '';
+}
+
+resetButton.addEventListener('click', resetGame);
+
+buttons.forEach((button, i) => {
+  button.addEventListener('click', () => {
+    playRound(options[i], computerPlay());
+  });
+});
+
+/*
+In our UI, the player should be able to play the game
+by clicking on buttons rather than typing their answer in
+a prompt.
+a. For now, remove the logic that plays exactly five rounds.
+
+b. Create three buttons, one for each selection. Add an
+event listener to the buttons that calls your playRound
+function with the correct playerSelection every time a button
+is clicked. (you can keep the console.logs for this step)
+
+c. Add a div for displaying results and change all of your
+console.logs into DOM methods.
+
+d. Display the running score, and announce a winner of the
+game once one player reaches 5 points.
+*/
