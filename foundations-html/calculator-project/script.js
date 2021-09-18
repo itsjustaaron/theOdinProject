67c90ec2d9e74f0c6a245894c0cee28d069c3d98
Aaron Childers <aaron.lee.childers@gmail.com>
2021-09-18 00:45:00 -0400

    implement calculator functionality for single operations

--- foundations-html/calculator-project/script.js
+++ foundations-html/calculator-project/script.js
@@ -29,38 +29,68 @@ const operate = (n1, n2, operation) => operation(n1, n2);
 let displayValue = '0';
 let firstNumber;
 let secondNumber;
+let selectedOperation;
+
+function resetStorage(displayReset = false) {
+    displayReset ? displayValue = '0' : null;
+    firstNumber = undefined;
+    secondNumber = undefined;
+    selectedOperation = undefined;
+}
 
 // update display window based on user input
 function updateDisplay(e) {
-    console.log(e, e.target.textContent);
-
-    // operator logic
+    console.log(displayValue, firstNumber, secondNumber);
+    // check if input is an operator
     if (Object.keys(operators).includes(e.target.textContent)) {
-        // store the current displayValue for operation;
-        // since operations can be chained, make sure firstNumber
-        // is not already in use
+        // store the current displayValue for calculation;
+        // since operations can be chained,
+        // check if firstNumber is already in use
         if (!firstNumber) {
             firstNumber = +displayValue;
-            console.log(firstNumber);
+            displayValue = '0';
         } else {
             // perform current operation and update the display
-
+            // TODO: find bug in chaining multiple operations
+            // seems to get stuck using previous operator?
+            displayValue = operate(firstNumber, secondNumber, selectedOperation);
+            firstNumber = +displayValue;
+            secondNumber = undefined;
+            selectedOperation = undefined;
+            displayArea.textContent = displayValue;
         }
+
+        selectedOperation = operators[e.target.textContent];
+        return;
     }
 
     // equals === 'showtime, baby!'
     if (e.target.textContent === '=') {
-        // return displayArea.textContent = operate
+        if (!firstNumber || !secondNumber || !selectedOperation) {
+            return;
+        }
+
+        displayValue = operate(firstNumber, secondNumber, selectedOperation);
+
+        // update display before clearing storage values
+        displayArea.textContent = displayValue;
+        resetStorage();
+        return firstNumber = +displayValue;
     }
 
     if (e.target.textContent.toLowerCase() === 'clear') {
-        displayValue = '0';
+        // clear storage values before updating display
+        resetStorage(true);
         return displayArea.textContent = displayValue;
     }
 
     if (!displayValue || displayValue === '0') {
         displayValue = e.target.textContent;
-        console.log('display value was 0')
+
+        if (firstNumber) {
+            secondNumber = +displayValue;
+        }
+
         return displayArea.textContent = displayValue;
     }
 
@@ -74,7 +104,11 @@ function updateDisplay(e) {
     }
 
     if (displayValue && !Object.keys(operators).includes(e.target.textContent)) {
-        displayValue += e.target.textContent;
+        displayValue = +displayValue + +e.target.textContent;
+    }
+
+    if (firstNumber) {
+        secondNumber = +displayValue;
     }
 
     return displayArea.textContent = displayValue;
