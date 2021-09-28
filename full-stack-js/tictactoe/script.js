7c8324417093e394abf741117634495614645f5e
Aaron Childers <aaron.lee.childers@gmail.com>
2021-09-28 13:49:24 -0400

    add UI for game setup

--- full-stack-js/tictactoe/script.js
+++ full-stack-js/tictactoe/script.js
@@ -13,12 +13,18 @@ const gameBoard = (function() {
 
 // need a game controller module
 const gameController = (function () {
-    // dom manipulation logic
+    // grab any needed dom elements
     const gameTiles = document.querySelectorAll('.game-board__tile');
+    const pregameSteps = document.querySelectorAll('.details-modal__pregame__content > div');
     const resetButton = document.querySelector('.game-controls__reset');
-    const modalReset = document.querySelector('.postgame-modal__dialog__reset');
-    let computerAsPlayer2 = true;
+    const modalReset = document.querySelector('.details-modal__postgame__reset');
+    const continueButton = document.querySelector('.details-modal__pregame__continue');
+
+    // game options - TODO: refactor into object
+    let setupStepTracker = 1;
     let gameIsActive = true;
+    let validatingLastMove = false;
+    let computerAsPlayer2;
     let currentPlayer;
 
     // wait helper function
@@ -38,25 +44,37 @@ const gameController = (function () {
         gameTiles.forEach(tile => {
             tile.className = 'game-board__tile';
             tile.removeEventListener('click', handleGameplay);
-            tile.addEventListener('click', handleGameplay, {once: true});
+            tile.addEventListener('click', handleGameplay,);
         });
         // clear the canvas
         if (document.querySelector('canvas')) {
             document.querySelector('canvas').remove();
         }
-        // clear current player setting
+        // reset current gameplay settings
         currentPlayer = undefined;
+        computerAsPlayer2 = undefined;
         gameIsActive = true;
-        // hide modal
-        document.querySelector('.postgame-modal').classList.remove('postgame-modal--open');
+        validatingLastMove = false;
+        setupStepTracker = 1;
+
+        // reset pregame button text
+        continueButton.textContent = 'Continue';
+
+        // remove postgame modal (if open)
+        document.querySelector('.details-modal').classList.remove('details-modal--open-post');
+        //  make sure pregame steps are reset and open pregame modal
+        pregameSteps.forEach((step, i) => {
+            // we want to restart the modal at step1 every time
+            i > 0 ? step.classList.add('details-modal__pregame__content--hidden') : step.classList.remove('details-modal__pregame__content--hidden');
+        })
+        wait(250).then(() => toggleModal());
     }
 
     function makeComputerPlay() {
         console.log('Ok Computer!');
-        currentPlayer = Player('o', 'Computer');
-        // wait a second before playing
-        // to avoid awkward timing from
-        // instant computer play
+        currentPlayer = player2;
+        // wait a second before playing to avoid
+        // awkward timing from instant computer play
         wait(1000).then(() => {
             const { board } = gameBoard;
             const usedTiles = board.map((el, i) => !!el ? i : null);
@@ -79,29 +97,56 @@ const gameController = (function () {
         });
     }
 
-    function toggleModal() {
-        const modal = document.querySelector('.postgame-modal');
+    function toggleModal(gameOver = false) {
+        const modal = document.querySelector('.details-modal');
 
-        // if user clicks outside of modal or clicks close button, close modal
-        modal.addEventListener('click', (e) => {
-            e.stopPropagation();
+        // separate modals for pre and post game
+        // TODO: set class in variable and combine blocks to avoid repetition
+        if (!gameOver) {
+            modal.addEventListener('click', (e) => {
+                e.stopPropagation();
 
-            if (!e.target.closest('.postgame-modal__dialog') || e.target.closest('.postgame-modal__close')) {
-                console.log('Clicked outside of the modal - modal will close');
-                modal.classList.remove('postgame-modal--open');
-            } else {
-                console.log('Clicked inside the modal - modal will not close');
-            }
-        });
+                if (!e.target.closest('.details-modal__pregame') || e.target.closest('.details-modal__close')) {
+                    console.log('Clicked outside of the modal - modal will close');
+                    modal.classList.remove('details-modal--open-pre');
+                } else {
+                    console.log('Clicked inside the modal - modal will not close');
+                }
+            });
 
-        // allow modal to close with escape key
-        document.body.addEventListener('keydown', (e) => {
-            if (e.key === 'Escape') {
-                modal.classList.remove('postgame-modal--open');
-            }
-        }, { once: true });
+            // allow modal to close with escape key
+            document.body.addEventListener('keydown', (e) => {
+                if (e.key === 'Escape') {
+                    modal.classList.remove('details-modal--open-pre');
+                }
+            }, { once: true });
 
-        modal.classList.add('postgame-modal--open');
+            modal.classList.add('details-modal--open-pre');
+        }
+
+        // postgame modal
+        if (gameOver) {
+            // if user clicks outside of modal or clicks close button, close modal
+            modal.addEventListener('click', (e) => {
+                e.stopPropagation();
+
+                if (!e.target.closest('.details-modal__postgame') || e.target.closest('.details-modal__close')) {
+                    console.log('Clicked outside of the modal - modal will close');
+                    modal.classList.remove('details-modal--open-post');
+                } else {
+                    console.log('Clicked inside the modal - modal will not close');
+                }
+            });
+
+            // allow modal to close with escape key
+            document.body.addEventListener('keydown', (e) => {
+                if (e.key === 'Escape') {
+                    modal.classList.remove('details-modal--open-post');
+                }
+            }, { once: true });
+
+            modal.classList.add('details-modal--open-post');
+        }
     }
 
     function drawOnBoard(lineType, offset) {
@@ -161,7 +206,7 @@ const gameController = (function () {
 
             // write up outcome of game
             // TODO: extract to function
-            const modalContent = document.querySelector('.postgame-modal__dialog__content');
+            const modalContent = document.querySelector('.details-modal__postgame__content');
             let modalText;
 
             if (lineType !== 'tie') {
@@ -174,12 +219,18 @@ const gameController = (function () {
 
             modalContent.innerHTML = modalText;
 
-            wait(1000).then(() => toggleModal());
+            // call the modal with true so it knows the game is over
+            wait(750).then(() => toggleModal(true));
         }
     }
 
     function checkGameStatus(board) {
         if (board.filter(el => !!el).length < 5) {
+            // unlock board if computer isn't playing
+            if (!computerAsPlayer2 || currentPlayer.name === 'Computer') {
+                validatingLastMove = false;
+            }
+
             return console.log("Not enough placements to win");
         }
 
@@ -229,8 +280,14 @@ const gameController = (function () {
 
         if (!winner && board.filter(el => !!el).length === 9) {
             // It's a tie!
+            gameIsActive = false;
             drawOnBoard('tie', 0);
             return console.log("If at first you don't succeed, tie, try again...");
+        }
+
+        // unlock board if computer isn't playing
+        if (!computerAsPlayer2 || currentPlayer.name === 'Computer') {
+            validatingLastMove = false;
         }
 
         return console.log("continue playing");
@@ -239,21 +296,28 @@ const gameController = (function () {
     function handleGameplay(e) {
         e.stopPropagation();
 
-        currentPlayer = (computerAsPlayer2 || currentPlayer === player2 || !currentPlayer)
-        ? player1
-        : player2;
+        if (validatingLastMove) {
+            return console.log("wait your turn!");
+        }
 
-        const { position } = e.target.dataset;
         if (e.target.classList.length > 1) {
             return console.log("tile already in use", this);
         }
+
+        validatingLastMove = true;
+
+        const { position } = e.target.dataset;
+
+        currentPlayer = (currentPlayer === player2 || !currentPlayer)
+        ? player1
+        : player2;
 
         this.classList.add(`game-board__tile--${currentPlayer.gamePiece}`);
 
         gameBoard.board[position] = currentPlayer.gamePiece;
         console.log(gameBoard.board, currentPlayer.name);
         checkGameStatus(gameBoard.board);
-        if (computerAsPlayer2) {
+        if (computerAsPlayer2 && gameIsActive) {
             return makeComputerPlay();
         }
     }
@@ -267,25 +331,71 @@ const gameController = (function () {
         return setUpGame();
     }
 
+    function handlePlayerConfiguration(e) {
+        e.stopPropagation();
+        console.log(this);
+        console.log(`Alright kids, we're on step ${setupStepTracker}`);
+
+        // hide current step and show next step
+        function cycleSteps() {
+            document.querySelector(`.details-modal__pregame__content--step${setupStepTracker++}`).classList.add('details-modal__pregame__content--hidden');
+            document.querySelector(`.details-modal__pregame__content--step${setupStepTracker}`).classList.remove('details-modal__pregame__content--hidden');
+        }
+
+        if (setupStepTracker === 1) {
+            // see which game mode was selected and set options accordingly
+            const gameMode = document.querySelector('input[type="radio"]:checked');
+            computerAsPlayer2 = gameMode.value === 'ai';
+            console.log(`The computer ${computerAsPlayer2 ? 'will' : 'will not'} play as player 2`);
+
+            if (computerAsPlayer2) {
+                continueButton.textContent = "Let's play!";
+            }
+
+            return cycleSteps();
+        }
+
+        // assign player 1
+        if (setupStepTracker === 2) {
+            const nameOfPlayer = document.querySelector('input[name="player1"]').value;
+            player1 = Player('x', nameOfPlayer);
+
+            if (!computerAsPlayer2) {
+                continueButton.textContent = "Let's play!";
+                return cycleSteps();
+            } else {
+                player2 = Player('o', 'Computer');
+            }
+
+            // close modal for now - TODO: make a lets get started message before closing
+            return this.closest('.details-modal').classList.remove('details-modal--open-pre');
+        }
+
+        // assign player 2
+        if (setupStepTracker === 3) {
+            const nameOfPlayer = document.querySelector('input[name="player2"]').value;
+            player2 = Player('o', nameOfPlayer);
+
+            return this.closest('.details-modal').classList.remove('details-modal--open-pre');
+        }
+    }
+
     resetButton.addEventListener('click', handleReset);
     modalReset.addEventListener('click', handleReset);
-
-    // gameTiles.forEach(tile => tile.addEventListener('click', handleGameplay));
+    continueButton.addEventListener('click', handlePlayerConfiguration);
 
     return { renderBoard, drawOnBoard, setUpGame };
 })();
 
-// gameController.renderBoard(gameBoard.board);
-
 // need a player factory
 function Player(marker, playerName) {
-    // player factory logic
     const gamePiece = marker;
     const name = playerName ? playerName : `Player ${gamePiece.toUpperCase()}`;
     return { gamePiece, name };
 }
 
 // global logic
-const player1 = Player('x');
-const player2 = Player('o');
+// const player1 = Player('x');
+// const player2 = Player('o');
+let player1, player2;
 gameController.setUpGame();
